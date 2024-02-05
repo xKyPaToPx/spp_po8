0401bd0d39de8ae5398c0343d5afc89c2a384374
AlexaBailey <alexaserko@gmail.com>
2024-02-05 13:53:23 +0300

    Add Lab2 and Lab3

---
+++ reports/Serko/2/src/Lab2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("Pass arguments");
+    
+}
+else
+{
+
+    string str = "";
+    foreach (var item in args)
+    {
+        if (item == "-")
+        {
+            str += "\n";
+            str += Console.ReadLine();
+            str += "\n";
+
+        }
+        else
+        {
+            using (StreamReader reader = new StreamReader(item))
+            {
+                str += reader.ReadToEnd();
+
+            }
+
+        }
+
+    }
+
+    Console.WriteLine(str);
+}
+Console.ReadLine();
---
+++ reports/Serko/2/src/Lab2/Task2/bin/Release/net8.0/publish/mycat.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\SPFolder\SPP\Lab2\Task2\bin\Release\net8.0\publish\Task2.exe %*
