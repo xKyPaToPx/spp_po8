0f041904edd67e7a311e65e143df476fc58d1ff9
AlexaBailey <alexaserko@gmail.com>
2024-03-02 13:59:11 +0300

    Add 4,5,6,7 labs

---
+++ reports/Serko/4/src/Task2/Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2;
+namespace Task2
+{
+    internal class Word
+    {
+        private string word;
+
+        public Word(string word)
+        {
+            this.word = word;
+        }
+
+        public int Length()
+        {
+            return word.Length;
+        }
+
+        public string GetWord()
+        {
+            return word;
+        }
+    }
+
+}
---
+++ reports/Serko/5/Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+public interface IMobile
+{
+    void Call();
+    void Message();
+    void PhotoShoot();
+
+}
+
+public abstract class SamsungMobile : IMobile
+{
+    public abstract void Call();
+    public abstract void Message();
+    public abstract void PhotoShoot();
+
+
+    public void GPSNavigation()
+    {
+        Console.WriteLine("Enable GPS Navigation");
+    }
+   
+}
+
+public class Model : SamsungMobile
+{
+    public override void Call()
+    {
+        Console.WriteLine("Calling contact ...");
+    }
+
+    public override void Message()
+    {
+        Console.WriteLine("Staring chat with...");
+    }
+    public override void PhotoShoot()
+    {
+        Console.WriteLine("Turning on Camera...");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Model model = new Model();
+        model.Call();
+        model.Message();
+        model.GPSNavigation();
+        model.PhotoShoot();
+    }
+}
