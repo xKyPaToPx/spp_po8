17f4857b255c922d7647011bc6f14d1dadbff805
Anton Lobarev <loborew@gmail.com>
2024-03-18 12:47:18 +0300

    rename

--- reports/Lobarev/2/src/Lab2/Task1/obj/Release/net8.0/Task1.AssemblyInfo.cs
+++ reports/Lobarev/2/src/Lab2/Task1/obj/Release/net8.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67967a1e474294551993dfab05e09ee7a3443f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aaecc00bf557b9f61bc78e3c43baefc023a30e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
