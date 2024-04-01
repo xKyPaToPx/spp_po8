3d53d828a661ff5f96c57315ed78d5f832213597
Makarevich Ekaterina <99526931+Mkrshkk@users.noreply.github.com>
2024-04-01 14:37:15 +0300

    Fix task1

--- reports/Makarevich/2/src/task01/task01.java
+++ reports/Makarevich/2/src/task01/task01.java
@@ -16,35 +16,43 @@ public class task01 {
         BufferedReader br = new BufferedReader(reader);
         String line;
 
+        int lineNumber = 1;
         while ((line = br.readLine()) != null) {
-            text.append(line);
+            text.append(line).append("\n");
+            lineNumber++;
         }
         br.close();
 
         Vector<String> vec = new Vector<>();
         StringBuilder wordBuilder = new StringBuilder();
+        lineNumber = 1;
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
             if (Character.isLetterOrDigit(c)) {
                 wordBuilder.append(c);
             } else {
                 if (wordBuilder.length() > 0) {
-                    vec.add(wordBuilder.toString().toLowerCase());
+                    vec.add(wordBuilder.toString().toLowerCase() + ":" + lineNumber);
                     wordBuilder.setLength(0);
+                }
+                if (c == '\n') {
+                    lineNumber++;
                 }
             }
         }
         if (wordBuilder.length() > 0) {
-            vec.add(wordBuilder.toString().toLowerCase());
+            vec.add(wordBuilder.toString().toLowerCase() + ":" + lineNumber);
         }
 
         Map<String, Vector<Integer>> wordMap = new HashMap<>();
         for (int i = 0; i < vec.size(); i++) {
-            String word = vec.get(i);
+            String[] parts = vec.get(i).split(":");
+            String word = parts[0];
+            int lineNum = Integer.parseInt(parts[1]);
             if (!wordMap.containsKey(word)) {
                 wordMap.put(word, new Vector<>());
             }
-            wordMap.get(word).add(i + 1);
+            wordMap.get(word).add(lineNum);
         }
 
         for (Map.Entry<String, Vector<Integer>> entry : wordMap.entrySet()) {
