376c96029b42c4dd6e04d905d74bd0b6f1f62357
Anton Lobarev <loborew@gmail.com>
2024-04-15 10:39:11 +0300

    Add lab3 & delete .vs

---
+++ reports/Lobarev/3/src/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Task1;
+
+internal class Program
+{
+    static void Main()
+    {
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+        var set1 = new RealNumberSet(5);
+        set1.Add(1.1);
+        set1.Add(2.2);
+        set1.Add(3.3);
+
+        var set2 = new RealNumberSet(5);
+        set2.Add(4.4);
+        set2.Add(5.5);
+        set2.Add(3.3);
+
+        var unionSet = set1.Union(set2);
+        unionSet.Print(); // Выведет 1.1 2.2 3.3 4.4 5.5
+
+        Console.WriteLine(set1.Contains(2.2)); // Выведет true
+        Console.WriteLine(set1.Contains(6.6)); // Выведет false
+
+        set1.Remove(2.2);
+        set1.Print(); // Выведет 1.1 3.3
+    }
+}
---
+++ reports/Lobarev/3/src/Task2/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2;
+
+public class Room
+{
+    public int Rooms { get; set; }
+    public double Area { get; set; }
+    public int Floor { get; set; }
+    public string Address { get; set; }
+    public double RentPrice { get; set; }
+    public bool IsRented { get; set; }
+
+    public Room(int rooms, double area, int floor, string address, double rentPrice, bool isRented)
+    {
+        Rooms = rooms;
+        Area = area;
+        Floor = floor;
+        Address = address;
+        RentPrice = rentPrice;
+        IsRented = isRented;
+    }
+}
