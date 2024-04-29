3839001fba8811c48dcd6da1daad835d53b87864
Kodirov Behruz <99527212+KodirovBehruz@users.noreply.github.com>
2024-04-29 13:35:32 +0300

    Update Main.java

--- lab3/src/exercise1/Main.java
+++ lab3/src/exercise1/Main.java
@@ -1,22 +1,20 @@
-package lab3.exercise1;
-
 public class Main {
     public static void main(String[] args) {
         Triangle triangle1 = new Triangle(5, 5, 6);
         Triangle triangle2 = new Triangle(3, 4, 5);
 
-        System.out.println("Triangle 1:");
-        System.out.println("Perimeter: " + triangle1.calculatePerimeter());
-        System.out.println("Area: " + triangle1.calculateArea());
-        System.out.println("Exists: " + triangle1.exists());
+        System.out.println("Triangle 1: " + triangle1);
+        System.out.println("Perimeter: " + triangle1.determinePerimeter());
+        System.out.println("Area: " + triangle1.determineArea());
+        System.out.println("Exists: " + triangle1.isTriangleExists());
+        System.out.println("Are triangles equal? " + triangle1.isIsoscelesTriangle());
 
-        System.out.println("\nTriangle 2:");
-        System.out.println("Perimeter: " + triangle2.calculatePerimeter());
-        System.out.println("Area: " + triangle2.calculateArea());
-        System.out.println("Exists: " + triangle2.exists());
+        System.out.println("\nTriangle 2: " + triangle2);
+        System.out.println("Perimeter: " + triangle2.determinePerimeter());
+        System.out.println("Area: " + triangle2.determineArea());
+        System.out.println("Exists: " + triangle2.isTriangleExists());
+        System.out.println("Are triangles equal? " + triangle2.isIsoscelesTriangle());
 
-        System.out.println("\nTesting equals method:");
-        System.out.println("Are triangles equal? " + triangle1.equals(triangle2));
+        System.out.println("\nAre triangle1 and triangle2 equal?  " + triangle1.equals(triangle2));
     }
 }
-
