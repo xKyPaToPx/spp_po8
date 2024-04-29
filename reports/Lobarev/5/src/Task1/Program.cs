0edaf40c8b90c8370d3d4b6f425c3ec1540bba99
Anton Lobarev <loborew@gmail.com>
2024-04-29 11:23:40 +0300

    Add lab5

---
+++ reports/Lobarev/5/src/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1;
+
+internal class Program
+{
+    static void Main()
+    {
+        Engineer engineer = new();
+        Manager manager = new();
+
+        engineer.DoWork();
+        manager.DoWork();
+        manager.Manage();
+    }
+}
+
