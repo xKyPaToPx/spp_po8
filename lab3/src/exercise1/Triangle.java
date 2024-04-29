a83c51e8b077090d071216c667194d33906e05f2
Kodirov Behruz <99527212+KodirovBehruz@users.noreply.github.com>
2024-04-29 13:36:08 +0300

    Update Triangle.java

--- lab3/src/exercise1/Triangle.java
+++ lab3/src/exercise1/Triangle.java
@@ -1,69 +1,64 @@
-package lab3.exercise1;
-
 public class Triangle {
-    private double sideA;
-    private double sideB;
-    private double base;
+    private double sideA, sideB, sideC;
 
-    public Triangle(double sideA, double sideB, double base) {
+    public Triangle(double sideA, double sideB, double sideC) {
         this.sideA = sideA;
         this.sideB = sideB;
-        this.base = base;
+        this.sideC = sideC;
     }
 
     public double getSideA() {
         return sideA;
     }
+    public double getSideB() {
+        return sideB;
+    }
+    public double getSideC() {
+        return sideC;
+    }
 
     public void setSideA(double sideA) {
         this.sideA = sideA;
     }
-
-    public double getSideB() {
-        return sideB;
-    }
-
     public void setSideB(double sideB) {
         this.sideB = sideB;
     }
-
-    public double getBase() {
-        return base;
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
     }
 
-    public void setBase(double base) {
-        this.base = base;
+    public double determinePerimeter() {
+        return sideA + sideB + sideC;
+    }
+    public double determineArea() {
+        double semiperimeter = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC));
     }
 
-    public double calculatePerimeter() {
-        return sideA + sideB + base;
-    }
+    public boolean isTriangleExists() { return sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA; }
 
-    public double calculateArea() {
-        double height = Math.sqrt(sideA * sideA - (base * base / 4));
-        return 0.5 * base * height;
-    }
-
-    public boolean exists() {
-        return (sideA + sideB > base && sideA + base > sideB && sideB + base > sideA);
+    public boolean isIsoscelesTriangle() {
+        return (sideA == sideB) ||
+                (sideA == sideC) ||
+                (sideB == sideC);
     }
 
     @Override
     public String toString() {
-        return "Triangle{" +
-                "sideA=" + sideA +
-                ", sideB=" + sideB +
-                ", base=" + base +
-                '}';
+        return "{ sideA: " + sideA + ", sideB: " + sideB + ", sideC: " + sideC + " }";
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Triangle that = (Triangle) o;
-        return Double.compare(that.sideA, sideA) == 0 &&
-                Double.compare(that.sideB, sideB) == 0 &&
-                Double.compare(that.base, base) == 0;
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Triangle other = (Triangle) obj;
+        return sideA == other.sideA && sideB == other.sideB && sideC == other.sideC;
     }
 }
